471f2dfe071c866d8c5c2614824e5454f2185a04
Dandi Irwanto <dandi.ir20@gmail.com>
2025-09-24 16:39:00 +0700

    learn: Erasure type in generic java

---
+++ src/application/TypeErasureApp.java
@@ -0,0 +1,17 @@
+package application;
+
+import generic.MyData;
+
+public class TypeErasureApp {
+    public static void main(String[] args) {
+         MyData<String> myData = new MyData<>("Dandi");
+
+        // Type Erasure ketika dicompile aman tapi ketika dijalankan akan error jadi akan jadi berbahaya kalo tidak ada info generic
+        // MyData myData = new MyData<>("Dandi"); // contoh Type Erasure tanpa info generic
+
+        MyData<Integer> integerMyData = (MyData<Integer>) myData; // kalo ini di define dengan type erasure maka tidak ada error
+        Integer integer = integerMyData.getData();
+
+
+    }
+}
